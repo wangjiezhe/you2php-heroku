4627e2b467b8f57cb0dbe3e8adc339fcb7a77517
wangjiezhe <wangjiezhe@gmail.com>
2018-03-31 21:42:59 +0800

    Update config.php
    use getenv

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('APIKEY', getenv("APIKEY");//这里改成您的api key
+define('GJ_CODE', getenv("GJ_CODE");//国家代码
+define('SITE_NAME', getenv("SITE_NAME");//网站名字
+define('TITLENAME', getenv("TITLENAME");//网站名字
+define('EN2DEKEY', getenv("EN2DEKEY");//一个随机字符串，加解密KEY
+define('EMAIL', getenv("EMAIL");//邮箱
 ?>
