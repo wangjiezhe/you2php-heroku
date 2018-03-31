f68966cda50caf46c436041ec7bc5e83c79ff8a0
wangjiezhe <wangjiezhe@gmail.com>
2018-03-31 21:46:15 +0800

    Fix parentheses

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY");//这里改成您的api key
-define('GJ_CODE', getenv("GJ_CODE");//国家代码
-define('SITE_NAME', getenv("SITE_NAME");//网站名字
-define('TITLENAME', getenv("TITLENAME");//网站名字
-define('EN2DEKEY', getenv("EN2DEKEY");//一个随机字符串，加解密KEY
-define('EMAIL', getenv("EMAIL");//邮箱
+define('APIKEY', getenv("APIKEY"));//这里改成您的api key
+define('GJ_CODE', getenv("GJ_CODE"));//国家代码
+define('SITE_NAME', getenv("SITE_NAME"));//网站名字
+define('TITLENAME', getenv("TITLENAME"));//网站名字
+define('EN2DEKEY', getenv("EN2DEKEY"));//一个随机字符串，加解密KEY
+define('EMAIL', getenv("EMAIL"));//邮箱
 ?>
